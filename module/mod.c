12e82031bc17308ae293ed46b64a6c27896a3f15
FantasySR <192044614@qq.com>
2026-04-26 16:05:42 +0800

    更新 mod.c

--- module/mod.c
+++ module/mod.c
@@ -1,19 +1,14 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <accctl.h>
 
 static int mod_init(void)
 {
-    printk(KERN_INFO "Hello KPM from module!\n");
+    printk("<6>Hello KPM from module!\n");
     return 0;
 }
 
 static void mod_exit(void)
 {
-    printk(KERN_INFO "Goodbye KPM from module!\n");
+    printk("<6>Goodbye KPM from module!\n");
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
-
-MODULE_LICENSE("GPL");
+KPM_ENTRY(mod_init, mod_exit);
