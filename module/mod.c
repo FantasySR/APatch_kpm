db2fc0a4017f5718549590a39d3f3760c493cdc6
FantasySR <192044614@qq.com>
2026-04-26 15:49:39 +0800

    更新 mod.c

--- module/mod.c
+++ module/mod.c
@@ -1,18 +1,25 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-static int __init mod_init(void) {
-    printk(KERN_INFO "Hello from mod.kpm!\n");
+// KPM 相关宏（需要包含 KernelPatch 头文件）
+#include <kpm.h>
+
+static int __init mod_init(void)
+{
+    printk(KERN_INFO "Hello from my KPM module!\n");
     return 0;
 }
 
-static void __exit mod_exit(void) {
-    printk(KERN_INFO "Goodbye from mod.kpm!\n");
+static void __exit mod_exit(void)
+{
+    printk(KERN_INFO "Goodbye from my KPM module!\n");
 }
 
 module_init(mod_init);
 module_exit(mod_exit);
 
+// KPM 必需的宏
 KPM_NAME("my_mod");
 KPM_VERSION("1.0");
 KPM_LICENSE("GPL");
