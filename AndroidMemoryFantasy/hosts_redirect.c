f4b0da801db1a6893540c9b377169f0d0109bd42
FantasySR <192044614@qq.com>
2026-04-27 05:30:05 +0800

    更新 hosts_redirect.c

--- AndroidMemoryFantasy/hosts_redirect.c
+++ AndroidMemoryFantasy/hosts_redirect.c
@@ -1,4 +1,5 @@
-#include <kpmodule.h>
+#include <accctl.h>
+#include <linux/kernel.h>
 
 KPM_NAME("AndroidMemoryFantasy");
 KPM_VERSION("1.0");
@@ -6,13 +7,13 @@ KPM_LICENSE("GPL");
 
 static long my_init(const char *args, const char *event, void __user *reserved)
 {
-    printk("<6>AndroidMemoryFantasy: loaded\n");
+    printk(KERN_INFO "AndroidMemoryFantasy: loaded\n");
     return 0;
 }
 
 static long my_exit(void __user *reserved)
 {
-    printk("<6>AndroidMemoryFantasy: unloaded\n");
+    printk(KERN_INFO "AndroidMemoryFantasy: unloaded\n");
     return 0;
 }
 
