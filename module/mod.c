007de9173067b58bc2d703920b9e89f8a27ec3a8
FantasySR <192044614@qq.com>
2026-04-26 16:21:54 +0800

    更新 mod.c

--- module/mod.c
+++ module/mod.c
@@ -1,22 +1,23 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
+// KernelPatch KPM 模块的核心头文件
+#include <kpm.h>     
 
-// 入口函数
-static int my_init(void) {
+// 定义一个模块加载时执行的函数
+static int hello_init(void) {
+    // 使用内核打印函数，等级为 KERN_INFO
     printk(KERN_INFO "Hello KPM: Module loaded successfully!\n");
     return 0;
 }
 
-// 出口函数
-static void my_exit(void) {
+// 定义一个模块卸载时执行的函数
+static void hello_exit(void) {
     printk(KERN_INFO "Goodbye KPM: Module unloaded.\n");
 }
 
-// 使用标准的 KPM 入口宏进行注册
-KPM_INIT(my_init);
-KPM_EXIT(my_exit);
+// 使用 KPM 框架的宏来“绑定”上面定义的函数
+KPM_INIT(hello_init);
+KPM_EXIT(hello_exit);
 
-// 模块元数据
+// 提供模块的元数据，这些都是必需的
 KPM_NAME("my_module");
 KPM_VERSION("1.0");
 KPM_LICENSE("GPL");
