8239d71fe51f56070cd3c7f935fb193e9462c44f
FantasySR <192044614@qq.com>
2026-04-26 16:18:45 +0800

    更新 mod.c

--- module/mod.c
+++ module/mod.c
@@ -1,23 +1,22 @@
-#include <accctl.h>
-#include <linux/uaccess.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-#define TAG "my_module"
-
-static int my_init(void)
-{
-    printk(KERN_INFO "Hello from my KPM module!\n");
+// 入口函数
+static int my_init(void) {
+    printk(KERN_INFO "Hello KPM: Module loaded successfully!\n");
     return 0;
 }
 
-static void my_exit(void)
-{
-    printk(KERN_INFO "Goodbye from my KPM module!\n");
+// 出口函数
+static void my_exit(void) {
+    printk(KERN_INFO "Goodbye KPM: Module unloaded.\n");
 }
 
+// 使用标准的 KPM 入口宏进行注册
 KPM_INIT(my_init);
 KPM_EXIT(my_exit);
+
+// 模块元数据
 KPM_NAME("my_module");
 KPM_VERSION("1.0");
 KPM_LICENSE("GPL");
-
-MODULE_LICENSE("GPL");
